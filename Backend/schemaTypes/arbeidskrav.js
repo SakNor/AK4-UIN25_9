fa55428ef86bc161701fefa60996d904ed5ebe62
JennyVollsund <jennymv@hiof.no>
2026-04-22 13:56:30 +0200

    Lagde arbeidskrav.js og arbeidskravcard.js

---
+++ Backend/schemaTypes/arbeidskrav.js
@@ -0,0 +1,11 @@
+const arbeidskrav = {
+    name: "arbeidskrav",
+    title: "arbeidskrav",
+    type: "document",
+    fields: [
+        {name: "title", title: "Arbeidskrav:", type: "string"},
+        {name: "beskrivelse", title: "Beskrivelse", type: "string"}
+    ]
+}
+
+export default arbeidskrav

2d0f26159eed65c390bfe42296e29a34b712e676
JennyVollsund <jennymv@hiof.no>
2026-04-22 11:30:49 +0200

    Update client.js

--- Frontend/src/helpers/client.js
+++ Frontend/src/helpers/client.js
@@ -4,11 +4,7 @@ const client = createClient({
     projectId: "8q856t79",
     dataset: "production",
     useCdn: true, 
-<<<<<<< HEAD
-    apiVersion: "2026-04-13"
-=======
     apiVersion: "2026-04-20"
->>>>>>> Sakarias
 })
 
 export default client
